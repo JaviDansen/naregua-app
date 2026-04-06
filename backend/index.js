b47819bcfd3d8df49f207c441cde27690bb65bdb
JaviDansen <davi.ribeiro0618@gmail.com>
2026-04-05 23:50:07 -0300

    feat: endpoint /register funcionando

--- backend/index.js
+++ backend/index.js
@@ -8,3 +8,14 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000');
 });
+
+app.use(express.json());
+
+app.post('/register', (req, res) => {
+  const { nome, email, senha } = req.body;
+
+  res.json({
+    mensagem: 'Usuário cadastrado com sucesso',
+    usuario: { nome, email }
+  });
+});
