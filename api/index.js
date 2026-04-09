92f93d45e6c7b0a9acf6c15c1756b4dca46653a7
JaviDansen <davi.ribeiro0618@gmail.com>
2026-04-09 19:20:34 -0300

    fix: ajustar basePath da API Express no deploy da Vercel

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
 const serverless = require('serverless-http');
 const app = require('../backend/app');
 
-module.exports = serverless(app);
+module.exports = serverless(app, {
+  basePath: '/api/index'
+});
